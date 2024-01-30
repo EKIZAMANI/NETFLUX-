eda830bbb14d28f08e33fcfead0174361c91b187
EKIZAMANI <eki.zamani@gmail.com>
2024-01-30 20:10:16 +0700

    add: search feature

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
 
 import './App.css';
-import {getMovieList, searchMovie} from "./api"
+import { getMovieList, searchMovie } from "./api"
 import { useEffect, useState } from 'react';
 
 
 const App = () => {
-  const [popularMovie, setPopularMovie] = useState ([])
+  const [popularMovie, setPopularMovie] = useState([])
 
   // mengambil data result lalu set dengan variabel useState
-  useEffect(()=>{
+  useEffect(() => {
     getMovieList().then((results) => {
       setPopularMovie(results)
     })
-  }, [])  
+  }, [])
 
 
   // MERENDER DATA
   const PopularMovieList = () => {
     return popularMovie.map((movie, i) => {
       return (
-        <div key={i}>
-          <img className="Movie-img" 
+        <div key={i} className='Movie-wrapper'>
+          <img className="Movie-img"
             src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
-          />
-            <div className="Movie-tittle">{movie.title}</div>
-            <div className="Movie-rating">{movie.vote_average}</div>
-            <div className="Movie-date">{movie.release_date}</div>
+          ></img>
+          <div className="Movie-tittle">{movie.title}</div>
+          <div className="Movie-rating">Rating : {movie.vote_average}</div>
+          <div className="Movie-date">Release date : {movie.release_date}</div>
         </div>
       )
     })
   }
 
-  const search = ({q}) => {
-    console.log({q})
+  const search = async (q) => {
+    if (q.length > 3) {
+
+      const query = await searchMovie(q)
+      setPopularMovie(query.results)
+      console.log({ query: query })
+    }
+
   }
 
-  console.log({popularMovie: popularMovie})
+  // console.log({ popularMovie: popularMovie })
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Netflux Movie</h2>
+        <h2 className="head">Netflux</h2>
         <input placeholder="Pencarian film disini"
-          onChange = {({ target }) => search( target.value )}
+          onChange={({ target }) => search(target.value)}
         />
         <div className='Movie-container'>
-          <PopularMovieList/>          
+          <PopularMovieList />
         </div>
       </header>
     </div>
--- src/api.js
+++ src/api.js
@@ -4,13 +4,13 @@ const apiKey = process.env.REACT_APP_APIKEY
 const baseUrl = process.env.REACT_APP_BASEURL
 
 
-export const getMovieList = async ()  => {
+export const getMovieList = async () => {
     const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`)
     return movie.data.results
     // console.log({ movieList: movie })
 }
 
 export const searchMovie = async (q) => {
-    const search = await search.get(q)
-    return
+    const search = await axios.get(`${baseUrl}/search/movie?query=${q}&api_key=${apiKey}`)
+    return search.data
 }
